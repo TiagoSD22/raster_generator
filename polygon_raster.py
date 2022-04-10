4c9a296689efc74224ccc2ede2cc748a3a31dec4
Tiago Dionizio <tiago.dionizio@hp.com>
2022-04-10 20:45:24 -0300

    add method to draw hexagon

--- polygon_raster.py
+++ polygon_raster.py
@@ -59,3 +59,36 @@ class PolygonRaster:
                 matrix[point[0]][point[1]] = 255
 
         return matrix
+
+    def raster_hexagon(self, length):
+        v1 = (length // 2, 0)
+        v2 = (v1[0] + length, 0)
+        v3 = (v2[0] + length // 2, v2[1] + int(length * math.sqrt(3)/2))
+        v4 = (v3[0] - length // 2, v3[1] + int(length * math.sqrt(3)/2))
+        v5 = (v4[0] - length, v4[1])
+        v6 = (v5[0] - length // 2, v5[1] - int(length * math.sqrt(3)/2))
+
+        polygon_vertex_list = [v1, v2, v3, v4, v5, v6]
+
+        print("Hexagon vertex list: ", polygon_vertex_list)
+
+        edge1 = list(bresenham(v1[0], v1[1], v2[0], v2[1]))
+        edge2 = list(bresenham(v2[0], v2[1], v3[0], v3[1]))
+        edge3 = list(bresenham(v3[0], v3[1], v4[0], v4[1]))
+        edge4 = list(bresenham(v4[0], v4[1], v5[0], v5[1]))
+        edge5 = list(bresenham(v5[0], v5[1], v6[0], v6[1]))
+        edge6 = list(bresenham(v6[0], v6[1], v1[0], v1[1]))
+
+        polygon_lines = [edge1, edge2, edge3, edge4, edge5, edge6]
+
+        print("Hexagon raster edge list: ", polygon_lines)
+
+        w = 2*length + 1
+        h = int(math.sqrt(3)*length) + 1
+
+        matrix = np.zeros((w, h))
+        for edge in polygon_lines:
+            for point in edge:
+                matrix[point[0]][point[1]] = 255
+
+        return matrix
